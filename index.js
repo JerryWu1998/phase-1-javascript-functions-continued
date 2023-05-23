7f988ed3b95ffee534b4a5a167d3a3333603ab09
Yu Wu <yw3748@columbia.edu>
2023-05-23 07:42:07 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(value = 'roller-skate') {
+  return 'This Saturday, I want to ' + value + '!';
+}
+
+function mondayWork(value = 'go to the office') {
+  return 'This Monday, I will ' + value + '.';
+}
+
+function wrapAdjective(sym = '*') {
+  return function(role) {
+    return 'You are ' + sym + role + sym + '!';
+  }
+}
